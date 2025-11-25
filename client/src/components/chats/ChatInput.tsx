47efc463cd5b16ccf30a11acddd24515e0bac411
Shyam Patel <gurjarshyam0502@gmail.com>
2025-11-25 17:40:14 +0530

    testing

--- client/src/components/chats/ChatInput.tsx
+++ client/src/components/chats/ChatInput.tsx
@@ -41,7 +41,7 @@ function ChatInput() {
             <input
                 type="text"
                 className="w-full flex-grow rounded-md border-none bg-dark p-2 outline-none"
-                placeholder="Enter a message..."
+                placeholder="Enter a text..."
                 ref={inputRef}
             />
             <button
